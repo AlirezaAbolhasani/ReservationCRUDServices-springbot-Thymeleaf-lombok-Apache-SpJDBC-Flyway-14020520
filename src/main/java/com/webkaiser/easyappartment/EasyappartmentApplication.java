426c601fe49136954d3b105dd36fd138b8dcea03
Abolhasani <Alireza.ab.1111@gmail.com>
2023-08-11 19:30:52 +0330

    start

---
+++ src/main/java/com/webkaiser/easyappartment/EasyappartmentApplication.java
@@ -0,0 +1,13 @@
+package com.webkaiser.easyappartment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyappartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyappartmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webkaiser/easyappartment/EasyappartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webkaiser.easyappartment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyappartmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
